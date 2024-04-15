df09a7ab5d5d3ad38feee442bf73a31f6c638734
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-15 12:40:49 +0900

    Update 상품을 구매한 회원 비율 구하기.sql

--- 프로그래머스/Lv. 5/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/Lv. 5/상품을 구매한 회원 비율 구하기.sql
@@ -1,3 +1,4 @@
+# Version 1
 select
     date_format(b.sales_date, '%Y') as year,
     date_format(b.sales_date, '%m') as month,
@@ -7,3 +8,20 @@ from user_info a
     join online_sale b on a.user_id = b.user_id and a.joined like '2021%'
 group by 1, 2
 order by 1, 2
+
+
+# Version 2
+select
+    year(sales_date) as year
+    , month(sales_date) as month
+    , count(distinct b.user_id) as puchased_users
+    , round(count(distinct b.user_id)/total_cnt,1) as puchased_ratio
+from (
+    select user_id, count(*) over () as total_cnt
+    from user_info
+    where year(joined) = 2021
+) a
+    left outer join online_sale b on a.user_id = b.user_id
+where sales_date is not null
+group by 1,2
+order by 1,2
