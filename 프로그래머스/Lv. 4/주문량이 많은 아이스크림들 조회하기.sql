f0ca07aa86f2a1c3fd5096a07795bac87065d5bb
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-26 16:48:04 +0900

    Update 주문량이 많은 아이스크림들 조회하기.sql

--- 프로그래머스/Lv. 4/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/Lv. 4/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,6 +1,21 @@
+-- Version 1
 select coalesce(a.flavor, b.flavor) as flavor
 from first_half a
     join july b on a.flavor = b.flavor
 group by 1
 order by sum(a.total_order)+sum(b.total_order) desc
+limit 3
+
+
+-- Version 2
+select flavor
+from (
+    select flavor, total_order
+    from first_half
+    union all
+    select flavor, total_order
+    from july
+) temp
+group by 1
+order by sum(total_order) desc
 limit 3
