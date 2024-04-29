508e41a926afaeaf420b7e18b0dc103d8be76a18
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-29 17:00:53 +0900

    Update 보호소에서 중성화한 동물.sql

--- 프로그래머스/Lv. 4/보호소에서 중성화한 동물.sql
+++ 프로그래머스/Lv. 4/보호소에서 중성화한 동물.sql
@@ -1,5 +1,16 @@
+-- Version 1
 select a.animal_id, a.animal_type, a.name
 from animal_ins a
     join animal_outs b on a.animal_id = b.animal_id
 where a.sex_upon_intake like 'Intact%' and b.sex_upon_outcome not like 'Intact%'
+order by 1
+
+
+-- Version 2
+select a.animal_id, a.animal_type, a.name
+from animal_ins a
+    inner join animal_outs b on a.animal_id = b.animal_id
+where 1=1
+    and lower(a.sex_upon_intake) like '%intact%'
+    and regexp_like(lower(b.sex_upon_outcome), 'spayed|neutered')
 order by 1
