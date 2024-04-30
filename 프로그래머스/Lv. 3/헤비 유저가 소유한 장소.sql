830f2c4438a2b172ccd87193cc12edfeb25c9157
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-30 14:42:09 +0900

    Update 헤비 유저가 소유한 장소.sql

--- 프로그래머스/Lv. 3/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/Lv. 3/헤비 유저가 소유한 장소.sql
@@ -1,3 +1,4 @@
+-- Version 1
 select *
 from places
 where host_id in (
@@ -5,4 +6,16 @@ where host_id in (
     from places
     group by 1
     having count(*) > 1)
+order by 1
+
+
+-- Version 2
+select id, name, host_id
+from (
+    select *, count(*) over (partition by host_id) as cnt
+    from places
+    group by 1
+) tmp
+where 1=1
+    and cnt > 1
 order by 1
