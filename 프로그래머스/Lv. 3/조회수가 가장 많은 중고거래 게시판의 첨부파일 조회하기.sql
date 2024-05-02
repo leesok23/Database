ef721bbe03dfb5e01f49a23131c5504d947f5e0e
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-05-02 11:32:05 +0900

    Update 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql

--- 프로그래머스/Lv. 3/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/Lv. 3/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,5 +1,18 @@
+-- Version 1
 select concat('/home/grep/src/',a.board_id,'/',b.file_id,b.file_name,b.file_ext) as file_path
 from used_goods_board a
     join used_goods_file b on a.board_id = b.board_id
 where views = (select max(views) from used_goods_board)
 order by file_id desc
+
+
+-- Version 2
+select concat('/home/grep/src/',b.board_id,'/',b.file_id,b.file_name,b.file_ext) as file_path
+from (
+    select *, rank() over (order by views desc) as rnk
+    from used_goods_board
+) a
+    inner join used_goods_file b on a.board_id = b.board_id
+where 1=1
+    and a.rnk = 1
+order by 1 desc
