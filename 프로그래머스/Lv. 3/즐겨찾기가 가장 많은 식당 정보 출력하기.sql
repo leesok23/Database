5c58d1260596810bdd519e21556990117cf125c4
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-05-02 12:17:54 +0900

    Update 즐겨찾기가 가장 많은 식당 정보 출력하기.sql

--- 프로그래머스/Lv. 3/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/Lv. 3/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,7 +1,19 @@
+-- Version 1
 select food_type, rest_id, rest_name, favorites
 from rest_info
 where (food_type, favorites) in (
     select food_type, max(favorites)
     from rest_info
     group by 1)
+order by 1 desc
+
+
+-- Version 2
+select food_type, rest_id, rest_name, favorites
+from (
+    select *, rank() over (partition by food_type order by favorites desc) as rnk
+    from rest_info
+) tmp
+where 1=1
+    and rnk = 1
 order by 1 desc
