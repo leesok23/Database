93b324eb3b29a5ed5e38e63eecacef72c94b7c9f
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-05-03 12:06:10 +0900

    Update 오랜 기간 보호한 동물(1).sql

--- 프로그래머스/Lv. 3/오랜 기간 보호한 동물(1).sql
+++ 프로그래머스/Lv. 3/오랜 기간 보호한 동물(1).sql
@@ -1,6 +1,16 @@
+-- Version 1
 select a.name, a.datetime
 from animal_ins a
     left join animal_outs b on a.animal_id = b.animal_id
 where b.animal_id is null
 order by a.datetime
+limit 3
+
+
+-- Version 2
+select name, datetime
+from animal_ins
+where 1=1
+    and animal_id not in (select animal_id from animal_outs)
+order by datetime
 limit 3
